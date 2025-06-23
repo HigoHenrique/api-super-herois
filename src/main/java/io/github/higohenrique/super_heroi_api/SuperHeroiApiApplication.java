4a9c8e10d9df7802d67142ee3754bb94452720ad
Higo Henrique da Silva <silvahigo2@gmail.com>
2025-06-23 09:51:17 -0300

    inicialização do projeto

---
+++ src/main/java/io/github/higohenrique/super_heroi_api/SuperHeroiApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.higohenrique.super_heroi_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperHeroiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperHeroiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/higohenrique/super_heroi_api/SuperHeroiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.higohenrique.super_heroi_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperHeroiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
